6c9e5b56bbfd985959702f76dbf8a65cb49d92f0
WordlessEcho <wordless@echo.moe>
2023-04-06 23:01:03 +0800

    Add SpringBoot Gateway and test functions

---
+++ grainmall-gateway/src/test/java/moe/echo/grainmall/gateway/GrainmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.echo.grainmall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrainmallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
